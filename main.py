e8b5de9d7cfd7e1528d05f5eaf4d3d1013d606d2
Alqama <alqamaazeem@gmail.com>
2023-12-02 05:23:35 +0530

    Python learning

---
+++ main.py
@@ -0,0 +1,62 @@
+# Tutorial 4
+
+# print("Hello World", 5)
+# print(5)
+# print(9*80)
+
+# Tutorial 5
+
+"""
+Hey guys, this is a way to comment `Multi-Lines`
+Author: Alqama
+"""
+# print("Hey guys I am \"Alqama\"\nand I am learning python")
+
+# print("Hello World")
+# # This is a 'Single-Line Comment'
+# print("This is a print statement.")
+# print("Hello World !!!") # Printing Hello World!
+
+# print("Hello", 4,5,6, sep="/", end="=")
+# print("Alqama")
+
+# Tutorial 6
+
+a = True
+b = complex(6 , 5)
+number = 7980230005
+name = "Alqama"
+number1 = 9
+number2 = 7.2
+print(b)
+print(number)
+print(name)
+print(number + number1)
+
+# Check the type of a variable
+print("The type of Name is:", type(name))
+print("The type of Number is:", type(number))
+print("The type of Number is:", type(number2))
+print("The type of A is:", type(a))
+print("The type of B is:", type(b))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name": "Alqama", "age": 18, "canVote": True}
+print(dict1)
+
+# Tutorial 7 - Exercise 1: Calculator
+
+# Operators
+
+print(5+7) # Addition
+print(15-7) # Subtraction
+print(15*7) # Multiplication
+print(2**4) # Exponential
+print(15/7) # Division
+print(15//7) # Floor Division
+print(15%7) # Modulus
