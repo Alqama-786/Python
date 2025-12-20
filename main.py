1bc9bf0cdba2276855711ff06f9c90d0793b029b
Alqama <alqamaazeem@gmail.com>
2025-12-21 01:18:20 +0530

    File io reading and writing a file

--- main.py
+++ main.py
@@ -454,3 +454,45 @@ def variables():
     print(x,z) # Output: 10, 10, also x is not a local variable here it is global and we are changing it.
 
 variables()
+
+# File IO
+
+# READING A FILE
+
+f = open('main.txt', 'r')
+# print(f)
+text = f.read()
+print(text)
+f.close()
+
+# WRITING A FILE
+
+f = open('myfile.txt', 'a')
+f.write('Hello, world!')
+f.close()
+
+with open('myfile.txt', 'a') as f:
+  f.write("Hey I am inside with")
+
+# Reading & Writing a file line by line.
+
+f = open('myfile.txt', 'r')
+i = 0
+while True:
+  i = i + 1
+  line = f.readline()
+  if not line:
+    break
+  m1 = int(line.split(",")[0])
+  m2 = int(line.split(",")[1])
+  m3 = int(line.split(",")[2])
+  print(f"Marks of student {i} in Maths is: {m1*2}")
+  print(f"Marks of student {i} in English is: {m2*2}")
+  print(f"Marks of student {i} in SST is: {m3*2}")
+
+  print(line)
+
+f = open('myfile2.txt', 'w')
+lines = ['line 1\n', 'line 2\n', 'line 3\n']
+f.writelines(lines)
+f.close()
