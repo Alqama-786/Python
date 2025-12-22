18f86cb7c0ff5f13c820a2c653a8860b1699e3a4
Alqama <alqamaazeem@gmail.com>
2025-12-22 17:41:36 +0530

    seek(), tell() and other functions of file

--- main.py
+++ main.py
@@ -496,3 +496,12 @@ f = open('myfile2.txt', 'w')
 lines = ['line 1\n', 'line 2\n', 'line 3\n']
 f.writelines(lines)
 f.close()
+
+# seek(), tell() and other functions
+with open("main.txt", "r") as f:
+    # Move to the 10th byte in file
+    f.seek(10)
+    # Tells the current byte position which is 10 here
+    print(f.tell())
+    data = f.read() # Read 5 bytes from a file
+    print(data)
