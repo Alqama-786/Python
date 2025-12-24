205a2bf0e11a7f3a978cb35ce66add51067c70ea
Alqama <alqamaazeem@gmail.com>
2025-12-24 21:05:46 +0530

    Getters and setters

--- main.py
+++ main.py
@@ -597,3 +597,21 @@ def add(a, b):
 hello()
 # greet(add)(1, 2)
 add(1, 2)
+
+# Getters and Setters
+class MyClass:
+    def __init__(self, value):
+        self._value = value
+    show = lambda self: print(f"Value is {self._value}")
+
+    @property
+    def ten_value(self):
+        return 10 * self._value
+    
+    @ten_value.setter
+    def ten_value(self, new_value):
+        self._value = new_value/10
+    
+obj = MyClass(10)
+obj.ten_value = 86
+obj.show()
