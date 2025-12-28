2f66ae41f18d314a2d5a03b2c5b1dc329a2d73f1
Alqama <alqamaazeem@gmail.com>
2025-12-28 16:07:02 +0530

    Inheritance

--- main.py
+++ main.py
@@ -615,3 +615,22 @@ class MyClass:
 obj = MyClass(10)
 obj.ten_value = 86
 obj.show()
+
+# Inheritance
+class Employee:
+  def __init__(self, name, id):
+    self.name = name
+    self.id = id 
+
+  def showDetails(self):
+    print(f"The name of Employee: {self.id} is {self.name}")
+
+class Programmer(Employee):
+  def showLanguage(self):
+    print("The default langauge is Python")
+
+e1 = Employee("Alqama", 405)
+e1.showDetails()
+e2 = Programmer("Alkma", 410)
+e2.showDetails()
+e2.showLanguage()
